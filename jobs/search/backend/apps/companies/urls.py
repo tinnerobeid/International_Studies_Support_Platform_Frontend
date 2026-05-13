d02116afd7295b807cb2becb6c7982bc1178fcc0
Christina Obeid <74201129+tinnerobeid@users.noreply.github.com>
2026-05-13 18:21:32 +0900

    Delete jobs directory

--- jobs/search/backend/apps/companies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CompanyListCreateView, CompanyDetailView
-
-urlpatterns = [
-    path('', CompanyListCreateView.as_view(), name='company-list'),
-    path('<slug:slug>/', CompanyDetailView.as_view(), name='company-detail'),
-]
--- jobs/search/backend/apps/locations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegionListView, RegionDistrictsView
-
-urlpatterns = [
-    path('regions/', RegionListView.as_view(), name='region-list'),
-    path('regions/<int:region_id>/districts/', RegionDistrictsView.as_view(), name='region-districts'),
-]
